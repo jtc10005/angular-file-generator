8bb0dea7fbc941d4370f7a7500bb7f8853f6db01
Jay Culpepper <jay.culpepper@ldc.com>
2018-02-26 16:48:11 -0600

    trying to get selected folder path

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
-# afg README
+# angular-file-generator README
 
-This is the README for your extension "afg". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "angular-file-generator". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Generate angular 2+ components similar to CLI in vscode
 
-For example if there is an image subfolder under your extension project workspace:
+> ngGenerate
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+### Keyboad Shorcuts:
+#### Windows
+> ctrl+alt+g
+#### Mac
+> cmd+alt+g
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+* None
 
 ## Extension Settings
 
@@ -28,38 +30,10 @@ This extension contributes the following settings:
 * `myExtension.thing`: set to `blah` to do something
 
 ## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+* None
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
+ ### 0.0.1 - Inital Release
 
 -----------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,48 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri, commands as Commands } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext, folder: WorkspaceFolder) {
+
+
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "afg" is now active!');
+    console.log('Congratulations, your extension "angular-file-generator" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = Commands.registerCommand('extension.ngGenerate', () => {
         // The code you place here will be executed every time your command is executed
+        // let n1 = vscode.workspace.onDidChangeWorkspaceFolders(e => {
+        //     console.log(e);
+        // });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        //https://github.com/dkundel/vscode-new-file/tree/master/src
+        
+        let Name = '';
+        let test = Window.showInputBox({ placeHolder: 'name-component', prompt: 'Enter new component name.' })
+            .then(fn => {
+                // let success = await Commands.executeCommand('vscode.copyFilePath');
+                //if filename then check if '-component' text is included if its not then add it
+                if (fn) {
+                    Name = fn.indexOf('-component') > 0 ? fn : `${fn}-component`;
+                    console.log('component name: ', Name);
+                } else {
+                    Window.showErrorMessage('No filename entered. Cannot generate file.');
+                    throw new Error('No filename entered. Cannot continue.');
+                }
+                // let x = vscode.window.showWorkspaceFolderPick().then(res => console.log(res));
+                // console.log('commands', success);
+            });
+
+
     });
+
 
     context.subscriptions.push(disposable);
 }
